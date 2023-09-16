3f6bec028114d3b2eb88ef1084d65b6a85e67db8
DiarmaidMcDonald <>
2023-09-16 14:21:10 +0000

    update

--- run.py
+++ run.py
@@ -1,22 +1,19 @@
 import random
 
-# Creating an empty board for game board.
-
+# Creating an empty board for the game board.
 def create_board(size):
     return [['' for _ in range(size)] for _ in range(size)]
 
 # Print the game board.
-
 def print_board(board):
     for row in board:
         print(''.join(row))
-        print('Let the Game Begin')
+    print('Let the Game Begin.\n')
 
-# Put the computers battleship randomly on the board for each game.
-# 'B' is a battleship.
-
+# Put the computer's battleship randomly on the board for each game.
+# 'B' represents a battleship.
 def place_ships(board, num_ships):
-    for i in range(num_ships):
+    for _ in range(num_ships):
         while True:
             x = random.randint(0, len(board) - 1)
             y = random.randint(0, len(board) - 1)
@@ -25,12 +22,11 @@ def place_ships(board, num_ships):
                 break
 
 # Users guess for the location of a battleship.
-# Check if the guess is within the grid of the board. 
-
+# Check if the guess is within the grid of the board.
 def users_guess(board):
     while True:
         try:
-            guess = input("Enter guess(row col): ")
+            guess = input("Enter guess (row col): ")
             row, col = map(int, guess.split())
             if 0 <= row < len(board) and 0 <= col < len(board):
                 return row, col
